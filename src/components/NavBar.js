92e9e51fa25beb0721654760a31184936c7599d0
mpedrano <mpedrano@my.bcit.ca>
2023-11-18 23:26:02 -0800

    fix styling conflicts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,10 @@ export default function NavBar () {
         <div className="navBar">
             <div>
                 <img className='mainLogo' src={swipewiseman} alt="swipewiseman" style={{ width: '235px', height: '85px' }}/>
-                <div className='navButton'>
-                    <button className='signUp'>Sign Up</button>
-                    <button className='login'>Log in</button>
-                    <img/>
-                </div>
+            </div>
+            <div className='navButton'>
+                <button className='signUp'>Sign Up</button>
+                <button className='login'>Log in</button>
             </div>
         </div>
     )

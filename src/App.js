9cdddea3debebf5c6f7b620421e05356adba8d26
mpedrano <mpedrano@my.bcit.ca>
2023-11-28 21:43:17 -0800

    fix router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TransactionList from "./components/Transaction/TransactionList";
 import TransactionForm from "./components/Transaction/TransactionForm";
 import { TransactionContext } from "./data/TransactionContext";
@@ -5,15 +7,12 @@ import { BarChart } from "./components/Chart/Chart";
 import { useState } from "react";
 import NavBar from "./components/NavBar";
 import LandingPage from "./components/LandingPage";
-import "./styles.css";
-
-import { Routes, Route } from 'react-router-dom';
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
+import "./styles.css";
 
 export default function App() {
   const [editing, setEditing] = useState(null);
-
   const [transactions, setTransactions] = useState([]);
 
   function addTransaction(transaction) {
@@ -42,78 +41,99 @@ export default function App() {
     );
     setEditing(null);
   }
+
   return (
-    <div className="App">
-      <NavBar/>
+    <Router>
+      <div className="App">
+        <NavBar />
 
-      <div className="routes">
-        <Routes>
-          <Route path="SignUp" element={<SignUp/>} />
-          <Route path="Login" element={<Login/>} />
-          <Route path="Home" element={<App/>} />
-        </Routes>
-      </div>
-
-      <LandingPage/>
-      <div className="container">
-      <div className="header">
-        <span className="heading">John Doe</span>
-        <span className="subheading">Welcome back!</span>
-      </div>
-      <div className="totalBox">
-        <div className="topRow">
-          <p>Total Balance</p>
-          <p>Account: Savings</p>
+        <div className="routes">
+          <Routes>
+            <Route path="/SignUp" element={<SignUp />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="Home" element={<App/>} />
+          </Routes>
         </div>
-          <span className="total">$2,150.67</span>
-        <div className="bottomRow">
-          <p><span className="up">▲</span>Income: + $120.75</p>
-          <p><span className="down">▼</span>Expense: - $53.00</p>
+        <div className="container">
+          <div className="header">
+            <span className="heading">John Doe</span>
+            <span className="subheading">Welcome back!</span>
+          </div>
+          <div className="totalBox">
+            <div className="topRow">
+              <p>Total Balance</p>
+              <p>Account: Savings</p>
+            </div>
+            <span className="total">$2,150.67</span>
+            <div className="bottomRow">
+              <p>
+                <span className="up">▲</span>Income: + $120.75
+              </p>
+              <p>
+                <span className="down">▼</span>Expense: - $53.00
+              </p>
+            </div>
+          </div>
+          <TransactionContext.Provider
+            value={{
+              transactions,
+              setTransactions,
+              updateTransaction,
+              addTransaction,
+              removeTransaction,
+              setEditing,
+              editing
+            }}
+          >
+            {!editing ? (
+              <div className="container">
+                <div className="buttons">
+                  <button className="budget" onClick={() => setEditing("new")}>
+                    Set Budget
+                  </button>
+                  <button className="income" onClick={() => setEditing("new")}>
+                    Add Income
+                  </button>
+                  <button className="add" onClick={() => setEditing("new")}>
+                    + Add Transaction
+                  </button>
+                </div>
+                <div className="history">
+                  <span className="subheading">Transaction History</span>
+                  <button name="filter" className="filter">
+                    Filter ▼
+                  </button>
+                </div>
+                <TransactionList />
+              </div>
+            ) : (
+              <div className="container">
+                <div className="buttons">
+                  <button className="budget" onClick={() => setEditing("new")}>
+                    Set Budget
+                  </button>
+                  <button className="income" onClick={() => setEditing("new")}>
+                    Add Income
+                  </button>
+                  <button className="add" onClick={() => setEditing("new")}>
+                    + Add Transaction
+                  </button>
+                </div>
+                <TransactionForm />
+                <div className="history">
+                  <span className="subheading">Transaction History</span>
+                  <button name="filter" className="filter">
+                    Filter ▼
+                  </button>
+                </div>
+                <TransactionList />
+              </div>
+            )}
+          </TransactionContext.Provider>
+          <div className="subheading">Overview</div>
+          <BarChart />
         </div>
       </div>
-      <TransactionContext.Provider
-        value={{
-          transactions,
-          setTransactions,
-          updateTransaction,
-          addTransaction,
-          removeTransaction,
-          setEditing,
-          editing
-        }}
-      >
-        {!editing ? (
-          <div className="container">
-            <div className="buttons">
-              <button className="budget" onClick={() => setEditing("new")}>Set Budget</button>
-              <button className="income" onClick={() => setEditing("new")}>Add Income</button>
-              <button className="add" onClick={() => setEditing("new")}>+ Add Transaction</button>
-            </div>
-            <div className="history">
-              <span className="subheading">Transaction History</span>
-              <button name="filter"className="filter">Filter ▼</button>
-            </div>
-              <TransactionList />
-          </div>
-        ) : (
-          <div className="container">
-            <div className="buttons">
-              <button className="budget" onClick={() => setEditing("new")}>Set Budget</button>
-              <button className="income" onClick={() => setEditing("new")}>Add Income</button>
-              <button className="add" onClick={() => setEditing("new")}>+ Add Transaction</button>
-            </div>
-            <TransactionForm />
-            <div className="history">
-              <span className="subheading">Transaction History</span>
-              <button name="filter"className="filter">Filter ▼</button>
-            </div>
-            <TransactionList />
-          </div>
-        )}
-      </TransactionContext.Provider>
-      <div className="subheading">Overview</div>
-      <BarChart/>
-      </div>
-    </div>
+    </Router>
   );
 }

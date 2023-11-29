43206f2e80ab9fd9695eacf119fbfd6ee372bb06
tham346 <thamashamara003@gmail.com>
2023-11-29 00:18:31 -0800

    budget functionality

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,15 +1,33 @@
 import React from "react";
 import SetBudget from "./SetBudget";
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export default function Budget(){
+    const now = 0;
     return(
         <div>
             <h1>Budget</h1>
             <h2>Total Balance</h2>
             <h2>$2,150.67</h2>
+            <ProgressBar now={now} label={`${now}%`} />;
             <p>Spent:</p>
             <p>Budget:</p>
             <button className="setBudget" onPress={() => SetBudget()}>Set Budget</button>
         </div>
+        //This is the code for when a user has set up a budget
+        // const now = 60;
+        // return(
+        //     <div>
+        //         <h1>Budget</h1>
+        //         <h2>Total Balance</h2>
+        //         <h2>$2,150.67</h2>
+        //         <ProgressBar now={now} label={`${now}%`} />;
+        //         <p>Spent:</p> //logic to implement user's imput
+        //         <p>Budget:</p> //logic to implement user's input
+        //         <button className="setBudget" onPress={() => SetBudget()}>Set Budget</button>
+        //         <ProgressBar/>
+        //         <ProgressBar/>
+        //         <ProgressBar/>
+        //     </div>
     )
 }
--- src/components/Budget/SetBudget.js
+++ src/components/Budget/SetBudget.js
@@ -1,6 +1,8 @@
 import react from "react";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -16,6 +18,21 @@ export default function SetBudget() {
                 </label>
             </form>
             <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+            <div>
+                <DropdownButton id="dropdown-basic-button" title="Repeat...">
+                    <Dropdown.Item href="#/action-1">Never</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Month</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Year</Dropdown.Item>
+                </DropdownButton>                 
+            </div>
+            <div>
+            <DropdownButton id="dropdown-basic-button" title="Criteria...">
+                <Dropdown.Item href="#/action-1">Groceries</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Transportation</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Utilities</Dropdown.Item>
+            </DropdownButton> 
+            </div>
+            <button>Save</button>
         </div>
     )
 }
